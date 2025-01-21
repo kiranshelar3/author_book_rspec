e482cd07e281eb042d552447e52b69077b0c58dd
kiran <kiranshelar407@gmail.com>
2025-01-21 23:18:20 +0530

    remain models failing test

--- spec/models/book_author_spec.rb
+++ spec/models/book_author_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe BookAuthor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    author = Author.create(first_name: "Rabindranath", last_name: "Tagore", date_of_birth: "1941-07-08")
+    book = Book.create(title: "Gitanjalli")
+    book_author = BookAuthor.new(author: author, book: book)
+    expect(book_author).to be_valid
+  end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has many authors through book_authors" do
+    book = Book.create(title: "Gitanjali")
+    author = Author.create(first_name: "Rabindranath", last_name: "Tagore", date_of_birth: "1941-07-08")
+    book.authors << author
+
+    expect(book.authors).to include(author)
+  end
 end
